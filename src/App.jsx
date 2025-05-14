55bc7050a249a1e046a40e59db81831c9d8ca035
Ayush Mittal <mittalayush0703@gmail.com>
2025-05-14 19:47:15 +0530

    added the functionality of local storage to persist the data across sessions

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ import { TodoList } from './TodoList';
 import { Tododate } from './Tododate';
 
 function App() {
-  const [task, setTask] = useState([]);
+  const [task, setTask] = useState(()=>{
+    const rawTodos = localStorage.getItem("todolocal");
+    if(!rawTodos) return [];
+    
+    return JSON.parse(rawTodos);
+  });
   
   const handleFormChange = (inputValue) =>{
     const {id, content, checked} = inputValue;
@@ -16,6 +21,7 @@ function App() {
 
     setTask((prev) => [...prev, {id: id, content: content, checked: checked}]);
   }
+  localStorage.setItem("todolocal", JSON.stringify(task));
 
 
      // handling the delete button
